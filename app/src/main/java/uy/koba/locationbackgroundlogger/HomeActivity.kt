240c8bc0c448cf6d6a8016cb40cfd15009bfd44f
“Agustín <agurz@icloud.com>
2017-08-19 13:33:53 -0300

    Button to send .log file by email

--- app/src/main/java/uy/koba/locationbackgroundlogger/HomeActivity.kt
+++ app/src/main/java/uy/koba/locationbackgroundlogger/HomeActivity.kt
@@ -1,17 +1,20 @@
 package uy.koba.locationbackgroundlogger
 
+import android.Manifest
 import android.content.pm.PackageManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.location.Location
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.app.ActivityCompat.checkSelfPermission
+import android.support.v4.content.FileProvider
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.Manifest
+import android.view.View
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -19,6 +22,9 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+
+import java.io.File
+
 
 class HomeActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -69,6 +75,22 @@ class HomeActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(map: GoogleMap) {
         this.map = map
+    }
+
+    // children views handlers
+
+    fun emailButtonOnClick(view: View) {
+        val intent = Intent(Intent.ACTION_SEND)
+        val file = File(applicationContext.filesDir, "location.log")
+
+        with(intent) {
+            type = "text/plain"
+            addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            putExtra(Intent.EXTRA_SUBJECT, "[Koba] LocationBackgroundLogger")
+            putExtra(Intent.EXTRA_STREAM, FileProvider.getUriForFile(applicationContext, "${applicationContext.packageName}.fileprovider", file))
+        }
+
+        startActivityForResult(Intent.createChooser(intent, "Email log file"), 1)
     }
 
     // member methods
